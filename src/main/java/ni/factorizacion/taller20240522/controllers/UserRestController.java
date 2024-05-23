eeb9ea6af2fba7f970745aacf1a770c430f8b573
Jezer Mejía <jezer.mejia@icloud.com>
2024-05-22 18:55:01 -0600

    Add Token and User, besides JWT dependencies

--- src/main/java/ni/factorizacion/taller20240522/controllers/UserRestController.java
+++ src/main/java/ni/factorizacion/taller20240522/controllers/UserRestController.java
@@ -1,11 +1,13 @@
 package ni.factorizacion.taller20240522.controllers;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import ni.factorizacion.taller20240522.domain.dtos.GeneralResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
---
+++ src/main/java/ni/factorizacion/taller20240522/domain/entities/User.java
@@ -0,0 +1,29 @@
+package ni.factorizacion.taller20240522.domain.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+public class User {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    private String identifier;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Token> tokens;
+}
